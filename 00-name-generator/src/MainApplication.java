504b9e52cca96d5a4b1ec3c126e628af5fe5a12f
Ian Higa <ian.higa@sbcglobal.net>
2020-01-16 14:27:51 -0800

    Added simple messages for testing main function

---
+++ 00-name-generator/src/MainApplication.java
@@ -0,0 +1,13 @@
+
+
+public class MainApplication {
+	public static final String FILE_DIRECTORY = "data/";
+	public static final String FILE_EXTENSION = ".txt";
+	public static final String SYLLABLE_FILE = "syllables";
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the name generator!\n");
+		System.out.println("Choose an option:\n1) Generate a random name!\n2) Generate a name of a specific length!\n3) Exit");
+	}
+
+}
